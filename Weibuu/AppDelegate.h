aefcce469a94d6185e68a1607a2a4af775ff3238
andy.zhang <andyzhang26503@gmail.com>
2013-01-16 22:56:00 +0800

    Initial Commit

---
+++ Weibuu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weibuu
+//
+//  Created by zhang andy on 13-1-16.
+//  Copyright (c) 2013年 com.andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
